0a6f337bd8e2b35fbe720e250f73cc3d2908f354
Waldemar <waldemar.schwan@uswitch.com>
2019-04-08 23:57:23 +0100

    cleanup

--- src/mapseqql/core.clj
+++ src/mapseqql/core.clj
@@ -13,17 +13,6 @@
 (def mini-sql-parser
   (insta/parser mini-sql-grammar))
 
-(def example-query "select * from dataset;")
-
-(def example-dataset [{:character "Goku" :ki-level 10}
-                      {:character "Goku (Great Ape)" :ki-level 100}
-                      {:character "Emperor Pilaf" :ki-level 40}
-                      {:character "Shu" :ki-level 20}
-                      {:character "Mai" :ki-level 20}
-                      {:character "Oolong" :ki-level 10}
-                      {:character "Puar" :ki-level 10}
-                      {:character "Ox-King" :ki-level 900}])
-
 (defmulti parse first)
 (defmethod parse :string  [[_ s]] (trim s))
 (defmethod parse :keyword [[_ k]] (keyword (subs (trim k) 1)))
@@ -39,7 +28,7 @@
     (select-keys m cols)))
 
 (defn- apply-query
-  [[_ [_ _ & columns] [_ _ [_ table-name]]] data]
+  [[_query [_select _ & columns] [_from _ [_ table-name]]] data]
   (let [cols   (remove nil? (map parse columns))
         mapseq (get data (parse table-name))]
     (map (partial select-columns cols) mapseq)))
@@ -51,11 +40,16 @@
     (apply-query q data)))
 
 (comment
-  (re-matches #"[0-9\w]+" "1asdf23")
+  (def example-dataset [{:character "Goku" :ki-level 10}
+                        {:character "Goku (Great Ape)" :ki-level 100}
+                        {:character "Emperor Pilaf" :ki-level 40}
+                        {:character "Shu" :ki-level 20}
+                        {:character "Mai" :ki-level 20}
+                        {:character "Oolong" :ki-level 10}
+                        {:character "Puar" :ki-level 10}
+                        {:character "Ox-King" :ki-level 900}])
+
   (prn mini-sql-parser)
-  (mini-sql-parser "select * from datasets;")
-  (mini-sql-parser "select ")
-  (type sql-grammar)
 
   (execute "select * from ds;" {"ds" example-dataset})
   (execute "select :ki-level, :character from ds;" {"ds" example-dataset}))
